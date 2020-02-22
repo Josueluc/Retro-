bef650322a5c767d11c530023e1b5062e0869b60
Josueluc <51139759+Josueluc@users.noreply.github.com>
2020-02-22 03:39:54 -0500

    Add files via upload
    vale vrg git :C

--- Retro-/pedido/registrar.php
+++ Retro-/pedido/registrar.php
@@ -1,4 +1,3 @@
-
 <?php
 // Include config file
 require_once "config.php";
@@ -44,12 +43,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }
     
     // Validate password
-    if(empty(trim($_POST["password"]))){
+    if(empty(trim($_POST["confirm_password"]))){
         $password_err = "Por favor ingresa una contraseña.";     
-    } elseif(strlen(trim($_POST["password"])) < 6){
+    } elseif(strlen(trim($_POST["confirm_password"])) < 6){
         $password_err = "La contraseña al menos debe tener 6 caracteres.";
     } else{
-        $password = trim($_POST["password"]);
+        $password = trim($_POST["confirm_password"]);
     }
    
     
@@ -86,7 +85,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 ?>
 <!doctype html>
 <html lang="es">
-
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -143,10 +141,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <div class="row justify-content-center mt-3 m-1">
             <div class="col-md-6 col-sm-8 col-xl-4 col-lg-5 formulario">
 
-                <form action="login.php" method="POST">
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
 
                     <div class="form-group text-center ">
-                        <h1 class="text-light">Registrase</h1>
+                        <h1 class="text-light">Registrarse</h1>
                     </div>
                     <div class="form-group mx-sm-4 pt-1">
 
@@ -154,8 +152,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                     </div>
 
                     <div class="form-group mx-sm-4 pt-1 <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
-                    <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
-                    <span class="help-block"><?php echo $username_err; ?></span>
+                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                <span class="help-block"><?php echo $username_err; ?></span>
                     </div>
 
                     <div class="form-group mx-sm-4 pt-1">
@@ -179,8 +177,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                     </div>
                     
                     <div class="form-group mx-sm-4 pt-1 <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                    <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
-                    <span class="help-block"><?php echo $password_err; ?></span>
+                    <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
+                    <span class="help-block"><?php echo $confirm_password_err; ?></span>
                     </div>
                     <div class="form-group mx-sm-4 pt-1">
                         <input type="submit" class="btn btn-block ingresar" value="Crear Cuenta">
@@ -190,5 +188,5 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             </div>
         </div>
     </div>
-</body>
+  </body>
 </html>
